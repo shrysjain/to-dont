97243c5237a1a8cb8856bac2abcc9d72276c3345
Shreyas Jain <shrys.jain@gmail.com>
2024-07-01 15:31:20 -0400

    work on app

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const AddTask = () => {
+  return (
+  <div>
+    <button className="btn btn-primary w-full font-bold text-white">ADD ITEM <AiOutlinePlus size={18} /></button>
+  </div>
+  );
+};
+
+export default AddTask;
